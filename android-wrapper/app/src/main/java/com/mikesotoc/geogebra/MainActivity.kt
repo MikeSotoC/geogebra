3a28f9308d31dee7fee19c47ccf22783dc9a93fa
MikeSotoC <93928843+MikeSotoC@users.noreply.github.com>
2026-09-12 17:39:49 -0500

    Add WebView based GeoGebra Android activity

---
+++ android-wrapper/app/src/main/java/com/mikesotoc/geogebra/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.mikesotoc.geogebra
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class MainActivity : Activity() {
+    private lateinit var webView: WebView
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        webView = WebView(this)
+        setContentView(webView)
+
+        webView.webViewClient = WebViewClient()
+        webView.webChromeClient = WebChromeClient()
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.settings.allowFileAccess = true
+        webView.settings.allowContentAccess = true
+
+        // Initial bootstrap. Next step is packaging the compiled source/web
+        // distribution into app/src/main/assets for fully offline operation.
+        webView.loadUrl("https://www.geogebra.org/classic")
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onBackPressed() {
+        if (::webView.isInitialized && webView.canGoBack()) webView.goBack()
+        else super.onBackPressed()
+    }
+}
